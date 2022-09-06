7866f5328919840bc67356b47e3037c212888eae
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-09-06 16:02:13 +0200

    feat(signature) : refonte de la signature

--- src/App.js
+++ src/App.js
@@ -5,32 +5,75 @@ function App() {
 		<>
 			<table>
 				<tr>
-					<td className="td-empty"></td>
-					<td className="padding-left bold keryan">Keryan Sanié</td>
-				</tr>
-				<tr>
-					<td colSpan={2} className="td-skip-a-line"></td>
-				</tr>
-				<tr>
-					<td rowspan="8" className="td-img border-right-solid">
+					<td rowspan="5" className="td-img">
 						<img
 							class="dev-in-net"
-							src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/DEV-IN-NET%20(6).gif?alt=media&token=cf0e1e38-1a38-478a-9dab-bb1cbb14024d"
-							alt="Grapefruit slice atop a pile of other slices"
+							src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/DEV-IN-NET%20(16).gif?alt=media&token=0dd7405e-c439-448b-9a61-86080f982d46"
+							alt=""
 						/>
 					</td>
-					<td className="padding-left">
-						<span className="bold">Dirigeant</span>, Dev-in-net
+					<td className="bold keryan">Keryan Sanié</td>
+				</tr>
+				<tr>
+					<td className="font-size">
+						<span className="bold">Fondateur</span>
+						<span className="vertical-divider"></span>Développeur Frontend
 					</td>
 				</tr>
 				<tr>
-					<td className="padding-left">
-						<span className="bold">Freelance</span>, Intégrateur web |
-						Développeur Front-End
+					<td className="td-skip-a-line"></td>
+				</tr>
+				<tr>
+					<td className="font-size">
+						<a className="color-link" href="http://www.devinnet.fr/">
+							<span>www.devinnet.fr</span>
+						</a>
+						<span className="vertical-divider"></span>Tél: 06 34 17 20 14
 					</td>
 				</tr>
 				<tr>
-					<td className="padding-left">
+					<td className="font-size">48 Rue Andy Warhol, 34000 Montpellier</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td className="font-size center">
+						<a className="color-link-blue" href="mailto:contact@devinnet.fr">
+							<span>contact@devinnet.fr</span>
+							<span className="vertical-divider"></span>
+						</a>
+						<a
+							href="https://www.linkedin.com/company/dev-in-net/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="linkedin-devinnet center"
+								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/icons8-linkedin-entour%C3%A9-60%20(4).png?alt=media&token=1ef15397-cd7f-4359-a4de-909d615170db"
+								alt=""
+							/>
+						</a>
+						{/* <span className="vertical-divider"></span>{' '}
+					<span className="color-link-blue">linkedin/devinnet.com</span> */}
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td className="td-skip-a-line"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td className="">
+						<a
+							href="https://github.com/keryanS"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="logo"
+								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/icons8-github-60%20(2).png?alt=media&token=73932f29-6355-4574-8d93-2c4a611a683d"
+								alt=""
+							/>
+						</a>
 						<a
 							href="http://www.keryan.sanie.fr/"
 							target="_blank"
@@ -38,7 +81,7 @@ function App() {
 						>
 							<img
 								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/pictogram-4471660_640-min.png?alt=media&token=ca8985db-c0d1-486c-8c8b-0e812db162f3"
+								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/icons8-domaine-60%20(2).png?alt=media&token=1e33e577-ace0-4324-bd99-c68ff2610af2"
 								alt=""
 							/>
 						</a>
@@ -49,10 +92,73 @@ function App() {
 						>
 							<img
 								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/linkedin.png?alt=media&token=3b0fce58-2120-4906-85b7-4a4943edbbc9"
+								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/icons8-linkedin-entour%C3%A9-60%20(2).png?alt=media&token=50fd0fac-60f5-462f-820e-46add835052c"
 								alt=""
 							/>
 						</a>
+					</td>
+				</tr>
+			</table>
+			<table>
+				<tr>
+					<td rowspan="5" className="td-img">
+						<img
+							class="dev-in-net"
+							src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/DEV-IN-NET%20(16).gif?alt=media&token=0dd7405e-c439-448b-9a61-86080f982d46"
+							alt=""
+						/>
+					</td>
+					<td className="bold keryan">Kevin Kotcherga</td>
+				</tr>
+				<tr>
+					<td className="font-size">
+						<span className="bold">Dev-in-net</span>
+						<span className="vertical-divider"></span>Développeur Frontend
+					</td>
+				</tr>
+				<tr>
+					<td className="td-skip-a-line"></td>
+				</tr>
+				<tr>
+					<td className="font-size">
+						<a className="color-link" href="http://www.devinnet.fr/">
+							<span>www.devinnet.fr</span>
+						</a>
+						<span className="vertical-divider"></span>Tél: 06 34 17 20 14
+					</td>
+				</tr>
+				<tr>
+					<td className="font-size">48 Rue Andy Warhol, 34000 Montpellier</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td className="font-size center">
+						<a className="color-link-blue" href="mailto:contact@devinnet.fr">
+							<span>contact@devinnet.fr</span>
+							<span className="vertical-divider"></span>
+						</a>
+						<a
+							href="https://www.linkedin.com/company/dev-in-net/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<img
+								className="linkedin-devinnet center"
+								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/icons8-linkedin-entour%C3%A9-60%20(4).png?alt=media&token=1ef15397-cd7f-4359-a4de-909d615170db"
+								alt=""
+							/>
+						</a>
+						{/* <span className="vertical-divider"></span>{' '}
+					<span className="color-link-blue">linkedin/devinnet.com</span> */}
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td className="td-skip-a-line"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td className="">
 						<a
 							href="https://github.com/keryanS"
 							target="_blank"
@@ -60,174 +166,20 @@ function App() {
 						>
 							<img
 								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/github.png?alt=media&token=2dff6b1e-35a8-474f-989d-853c9697b982"
-								alt=""
-							/>
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td className="td-skip-a-line"></td>
-				</tr>
-				<tr>
-					<td>
-						<span className="bold padding-left">Dev-in-net</span>
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						48 Rue Andy Warhol, 34000 Montpellier
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						06 34 17 20 14 | keryan.sanie@gmail.com
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						<a href="https://devinnet.fr/" target="_blank" rel="noreferrer">
-							<img
-								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/pictogram-4471660_640-min.png?alt=media&token=ca8985db-c0d1-486c-8c8b-0e812db162f3"
+								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/icons8-github-60%20(2).png?alt=media&token=73932f29-6355-4574-8d93-2c4a611a683d"
 								alt=""
 							/>
 						</a>
 						<a
-							href="https://www.linkedin.com/company/dev-in-net/"
+							href="https://www.linkedin.com/in/keryansanie/"
 							target="_blank"
 							rel="noreferrer"
 						>
 							<img
 								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/linkedin.png?alt=media&token=3b0fce58-2120-4906-85b7-4a4943edbbc9"
+								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/icons8-linkedin-entour%C3%A9-60%20(2).png?alt=media&token=50fd0fac-60f5-462f-820e-46add835052c"
 								alt=""
 							/>
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td className="td-skip-a-line"></td>
-				</tr>
-				<tr>
-					<td className="td-empty"></td>
-					<td className="padding-left">
-						<a
-							href="https://devinnet.fr"
-							target="_blank"
-							rel="noreferrer"
-							className="link"
-						>
-							<p>https://devinnet.fr</p>
-						</a>
-					</td>
-				</tr>
-			</table>
-			<table>
-				<tr>
-					<td className="td-empty"></td>
-					<td className="padding-left bold keryan">Kevin Kotcherga</td>
-				</tr>
-				<tr>
-					<td colSpan={2} className="td-skip-a-line"></td>
-				</tr>
-				<tr>
-					<td rowspan="8" className="td-img border-right-solid">
-						<img
-							class="dev-in-net"
-							src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/DEV-IN-NET%20(6).gif?alt=media&token=cf0e1e38-1a38-478a-9dab-bb1cbb14024d"
-							alt="Grapefruit slice atop a pile of other slices"
-						/>
-					</td>
-					<td className="padding-left">
-						<span className="bold">Développeur Frontend</span>, Dev-in-net
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						<span className="bold">Freelance</span>, Intégrateur web |
-						Développeur Front-End
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						<a
-							href="https://www.linkedin.com/in/kevinkotcherga/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img
-								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/linkedin.png?alt=media&token=3b0fce58-2120-4906-85b7-4a4943edbbc9"
-								alt=""
-							/>
-						</a>
-						<a
-							href="https://github.com/kevinkotcherga"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img
-								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/github.png?alt=media&token=2dff6b1e-35a8-474f-989d-853c9697b982"
-								alt=""
-							/>
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td className="td-skip-a-line"></td>
-				</tr>
-				<tr>
-					<td>
-						<span className="bold padding-left">Dev-in-net</span>
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						48 Rue Andy Warhol, 34000 Montpellier
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						06 34 17 20 14 | keryan.sanie@gmail.com
-					</td>
-				</tr>
-				<tr>
-					<td className="padding-left">
-						<a href="https://devinnet.fr/" target="_blank" rel="noreferrer">
-							<img
-								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/pictogram-4471660_640-min.png?alt=media&token=ca8985db-c0d1-486c-8c8b-0e812db162f3"
-								alt=""
-							/>
-						</a>
-						<a
-							href="https://www.linkedin.com/company/dev-in-net/"
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img
-								className="logo"
-								src="https://firebasestorage.googleapis.com/v0/b/signature-email-efddf.appspot.com/o/linkedin.png?alt=media&token=3b0fce58-2120-4906-85b7-4a4943edbbc9"
-								alt=""
-							/>
-						</a>
-					</td>
-				</tr>
-				<tr>
-					<td className="td-skip-a-line"></td>
-				</tr>
-				<tr>
-					<td className="td-empty"></td>
-					<td className="padding-left">
-						<a
-							href="https://devinnet.fr"
-							target="_blank"
-							rel="noreferrer"
-							className="link"
-						>
-							<p>https://devinnet.fr</p>
 						</a>
 					</td>
 				</tr>
